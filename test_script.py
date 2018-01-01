21d728508d3521ee31c538ab1f75aff35f2f8754
erictempleton1 <erictempleton1@gmail.com>
2018-01-01 11:17:23 -0500

    general cleanup and removing while loop

--- test_script.py
+++ test_script.py
@@ -17,34 +17,30 @@ GPIO.setmode(GPIO.BCM)
 
 # initiate list with pin gpio pin numbers
 
-gpioList = [26, 19, 13, 06, 12, 16, 20, 21]
+gpio_list = [26, 19, 13, 06, 12, 16, 20, 21]
 
-for i in gpioList:
+for i in gpio_list:
     GPIO.setup(i, GPIO.OUT)
     GPIO.output(i, GPIO.HIGH)
 
 # Sleep time variables
 
-sleepTimeShort = 0.2
-sleepTimeLong = 0.1
+sleep_time_short = 0.2
+sleep_time_long = 0.1
 
 # MAIN LOOP =====
 # ===============
 
 try:
-    while True:
-        for i in gpioList:
-            GPIO.output(i, GPIO.LOW)
-            time.sleep(sleepTimeShort);
-            GPIO.output(i, GPIO.HIGH)
-            time.sleep(sleepTimeLong);
-
-
-# End program cleanly with keyboard
+    for i in gpio_list:
+        GPIO.output(i, GPIO.LOW)
+        time.sleep(sleep_time_short)
+        GPIO.output(i, GPIO.HIGH)
+        time.sleep(sleep_time_long)
+        time.sleep(2)
 except KeyboardInterrupt:
-    print " Quit"
+    print "Ending and cleaning up.."
 
     # Reset GPIO settings
-
     GPIO.cleanup()
-
+    print "Done."
